c162bb7ecbccbd3d6594a30b3e471eb792fe3352
accup <32096121+accup@users.noreply.github.com>
2022-04-29 21:52:48 +0900

    rules_cardページ準備

---
+++ src/pages/rules_card/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import styles from "./index.module.scss"
+</script>
+
+<template>
+  Rules card準備中
+</template>
+
+<style lang="scss">
+@use "./index_global.module";
+</style>
--- src/router.ts
+++ src/router.ts
@@ -5,6 +5,10 @@ const routes = [
     path: '',
     component: () => import('@/pages/index.vue'),
   },
+  {
+    path: '/rules_card',
+    component: () => import('@/pages/rules_card/index.vue'),
+  },
 ]
 
 export default createRouter({
