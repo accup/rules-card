67c777728ad88fbfa3a1da6c3d838f5dfb09d4ad
accup <32096121+accup@users.noreply.github.com>
2022-05-05 13:56:23 +0900

    Rules cardのリンクをトップページに追加

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,8 +1,26 @@
 <script setup lang="ts">
 import styles from './index.module.scss'
+
+interface Page {
+  url: string
+  content: string
+}
+
+const pages: Page[] = [
+  {
+    url: '/rules_card/',
+    content: 'Rules Card',
+  },
+]
 </script>
 
-<template>トップページ準備中</template>
+<template>
+  <ul>
+    <a v-for="page in pages" :href="page.url">
+      <li>{{ page.content }}</li>
+    </a>
+  </ul>
+</template>
 
 <style lang="scss">
 @use './index_global.module';
